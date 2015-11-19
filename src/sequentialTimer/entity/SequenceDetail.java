5566183de09408460aff25e389be182fd820e972
jw8mc <jwilliams8@madisoncollege.edu>
2015-11-19 00:53:04 -0600

    tweaking database some more, added the JDBC connection library that keeps going missing

--- src/sequentialTimer/entity/SequenceDetail.java
+++ src/sequentialTimer/entity/SequenceDetail.java
@@ -8,16 +8,19 @@ package sequentialTimer.entity;
  */
 public class SequenceDetail {
 
+    private int id;
     private int timerSequenceId;
     private int sequentialTimerId;
-    private int preceedingTimerId;
+    private int precedingTimerId;
 
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
     public int getTimerSequenceId() { return timerSequenceId; }
     public void setTimerSequenceId(int timerSequenceId) { this.timerSequenceId = timerSequenceId; }
     public int getSequentialTimerId() { return sequentialTimerId; }
     public void setSequentialTimerId(int sequentialTimerId) { this.sequentialTimerId = sequentialTimerId; }
-    public int getPreceedingTimerId() { return preceedingTimerId; }
-    public void setPreceedingTimerId(int preceedingTimerId) { this.preceedingTimerId = preceedingTimerId; }
+    public int getPrecedingTimerId() { return precedingTimerId; }
+    public void setPrecedingTimerId(int precedingTimerId) { this.precedingTimerId = precedingTimerId; }
 
     /**
      * Empty constructor that takes no parameters.
@@ -28,8 +31,13 @@ public class SequenceDetail {
      * Constructor that takes three parameters to populate the data object.
      * @param timerSequenceId       int that represents the specific record
      * @param sequentialTimerId     int that represents the timer to call
-     * @param preceedingTimerId     int that represents which timer this record should follow
+     * @param precedingTimerId      int that represents which timer this record should follow
      */
+    public SequenceDetail(int timerSequenceId, int sequentialTimerId, int precedingTimerId) {
+        this.timerSequenceId = timerSequenceId;
+        this.sequentialTimerId = sequentialTimerId;
+        this.precedingTimerId = precedingTimerId;
+    }
 
     /**
      * Returns a string with object information.
@@ -37,8 +45,9 @@ public class SequenceDetail {
      */
     @Override
     public String toString() {
-        return System.lineSeparator() + "Record ID: " + timerSequenceId
+        return System.lineSeparator() + "Record ID: " + id
+                +System.lineSeparator() + "Belongs to sequence: " + timerSequenceId
                 + System.lineSeparator() + "Sequential Timer ID: " + sequentialTimerId
-                + System.lineSeparator() + "Follows: " + preceedingTimerId;
+                + System.lineSeparator() + "Follows: " + precedingTimerId;
     }
 }
